4a09e39c706d24aaaa9b4d225e63b3eaff3bced6
Adrenaline189 <luminexenergyglobal@gmail.com>
2025-09-24 19:57:32 +0700

    Update prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,15 @@
-// src/lib/prisma.ts
 import { PrismaClient, Prisma } from "@prisma/client";
 
 const log: Prisma.LogLevel[] = ["warn", "error"];
 
+// เขียน/ทรานแซกชัน → ใช้ direct (DATABASE_URL)
 export const prismaWrite = new PrismaClient({
   log,
-  datasources: { db: { url: process.env.DATABASE_URL! } }, // direct (no -pooler)
+  datasources: { db: { url: process.env.DATABASE_URL! } },
 });
 
+// อ่าน/health → ใช้ pooled (DATABASE_POOL_URL หรือ READONLY_POOL_URL)
 export const prismaRead = new PrismaClient({
   log,
-  datasources: { db: { url: process.env.READONLY_POOL_URL || process.env.DATABASE_POOL_URL! } }, // pooled
+  datasources: { db: { url: process.env.READONLY_POOL_URL || process.env.DATABASE_POOL_URL! } },
 });
