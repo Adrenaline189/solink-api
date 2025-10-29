22aa00dc1424660094e38c2a79ee7c5d0c47ad39
Adrenaline189 <luminexenergyglobal@gmail.com>
2025-10-29 20:55:23 +0700

    Update prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,6 @@
 // src/lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as { __prisma?: PrismaClient };
+export const prisma = new PrismaClient();
 
-export const prisma =
-  globalForPrisma.__prisma ??
-  new PrismaClient({
-    log: process.env.NODE_ENV === "production" ? [] : ["query", "error", "warn"],
-  });
-
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.__prisma = prisma;
-}
+export default prisma;
